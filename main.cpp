aad32a27b4a9b5ba25ec98fc5516f8b857c70f1f
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-04-10 00:06:11 -0800

    Forgot to mention that I moved all tests into their own function in main.cpp. Main can now take a filename to store PostScript code into.

--- main.cpp
+++ main.cpp
@@ -17,14 +17,15 @@ void writeTests(ofstream & out)
 {
 	const string nextSection = "showpage\n%****************************************%\n";
 	Rectangle myRectangle(40, 50);
-	Circle myCircle(30);
 	Spacer mySpacer(40, 50);
 
 	Polygon myPolygon(13, 50);
 
-	Square mySquare(200);
+	Square mySquare(inches(2));
 
 	Triangle myTriangle(inches(4));
+	
+	Circle myCircle(myTriangle.getHeight()/2);
 	
 	Rotated myRotated(&myTriangle,180);
 	vector<BasicShapes *>  layeredshapes = {&myRotated, &myTriangle},
