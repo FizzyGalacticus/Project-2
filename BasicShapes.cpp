5431df2a692c791ee7c5fbfd587e2b7023b058ff
Igor <techpro071989@yahoo.com>
2014-04-13 21:12:19 -0800

    Refactored BasicShapes Class and all of its derived classes

--- BasicShapes.cpp
+++ BasicShapes.cpp
@@ -7,3 +7,169 @@
 //
 
 #include "BasicShapes.h"
+
+BasicShapes::~BasicShapes(){};
+
+Rectangle::Rectangle(double width, double height):_width(width), _height(height){}
+
+const double & Rectangle::getHeight() const
+{
+    return _height;
+}
+
+const double & Rectangle::getWidth() const
+{
+    return _width;
+}
+
+string Rectangle::draw()
+{
+    string height = to_string(_height);
+    string width = to_string(_width);
+    string leftSide = "0 " + height + " rlineto \n";
+    string top = width + " 0 rlineto \n";
+    string rightSide = "0 -" + height + " rlineto \n";
+    return "newpath\n0 0 moveto\n" + leftSide + top + rightSide + "closepath\nstroke\n";
+}
+
+Polygon::~Polygon(){};
+
+Polygon::Polygon(double numSides, double sideLength):_numberOfSides(numSides), _sideLength(sideLength)
+{
+    if(fmod(_numberOfSides, 2) == 1)
+    {
+        _height = _sideLength*(1+cos(M_PI/_numberOfSides))/(2*sin(M_PI/_numberOfSides));
+        _width = (_sideLength * sin(M_PI*(_numberOfSides-1)/(2*_numberOfSides)))/(sin(M_PI/_numberOfSides));
+    }
+    else if(fmod(_numberOfSides, 4) == 0)
+    {
+        _height = _sideLength*(cos(M_PI/_numberOfSides))/(sin(M_PI/_numberOfSides));
+        _width = (_sideLength*cos(M_PI/_numberOfSides))/(sin(M_PI/_numberOfSides));
+    }
+    else
+    {
+        _height = _sideLength*(cos(M_PI/_numberOfSides))/(sin(M_PI/_numberOfSides));
+        _width = _sideLength/(sin(M_PI/_numberOfSides));
+    }
+}
+
+const double & Polygon::getHeight() const
+{
+    return _height;
+}
+
+const double & Polygon::getWidth() const
+{
+    return _width;
+}
+
+string Polygon::draw()
+{
+    string sideLength = to_string(_sideLength);
+    string numberOfSides = to_string(int(_numberOfSides));
+    
+    double rotationalVariable = 180 - (((_numberOfSides - 2) * 180) /_numberOfSides);
+    
+    
+    double startingPos = (_width - _sideLength)/2;
+    string start = "newpath\n";
+    string translate = to_string(int(startingPos)) + " 0 translate \n";
+    string drawPolygon = "0 1 " + numberOfSides + "{\n0 0 moveto\n" +
+    sideLength + " 0 lineto\ncurrentpoint translate\n" +
+    to_string(rotationalVariable) + " rotate\n}for \nstroke \n";
+    
+    return "gsave \n" + start + translate + drawPolygon + "grestore \n\n";
+}
+
+Square::Square(double sideLength):_sideLength(sideLength) {}
+
+const double & Square::getHeight() const
+{
+    return Polygon(4, _sideLength).getHeight();
+}
+
+const double & Square::getWidth() const
+{
+    return Polygon(4, _sideLength).getHeight();
+}
+
+string Square::draw()
+{
+    return Polygon(4, _sideLength).draw();
+}
+
+Triangle::Triangle(double sideLength):_sideLength(sideLength) {}
+
+const double & Triangle::getHeight() const
+{
+    return Polygon(3, _sideLength).getHeight();
+}
+
+const double & Triangle::getWidth() const
+{
+    return Polygon(3, _sideLength).getWidth();
+}
+
+string Triangle::draw()
+{
+    return Polygon(3, _sideLength).draw();
+}
+
+Spacer::Spacer(double width, double height):_width(width), _height(height){}
+
+const double & Spacer::getHeight() const
+{
+    return _height;
+}
+
+const double & Spacer::getWidth() const
+{
+    return _width;
+}
+
+string Spacer::draw()
+{
+    string height = to_string(_height);
+    string width = to_string(_width);
+    string leftSide = "0 " + height + " lineto \n";
+    string top = width + " " + height + " lineto \n";
+    string rightSide =  width + " 0 lineto \n";
+    return "newpath " + leftSide + top + rightSide + "closepath \n";
+    
+}
+
+Circle::Circle(double radius):_radius(radius){}
+const double & Circle::getHeight() const
+{
+    return _radius*2;
+}
+
+const double & Circle::getWidth() const
+{
+    return getHeight();
+}
+string Circle::draw()
+{
+    string radius = to_string(_radius);
+    return "newpath " + radius + " " + radius + " " + radius + " 0 360 arc stroke \n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- BasicShapes.h
+++ BasicShapes.h
@@ -12,14 +12,13 @@
 #include <iostream>
 #include <string>
 #include <math.h>
-
-
 using std::string;
 using std::to_string;
+
 class BasicShapes
 {
 public:
-    virtual ~BasicShapes() {};
+    virtual ~BasicShapes();
     virtual string draw() = 0;
     virtual const double & getHeight() const = 0;
     virtual const double & getWidth() const = 0;
@@ -29,27 +28,10 @@ public:
 class Rectangle : public BasicShapes
 {
 public:
-    Rectangle(double width, double height):_width(width), _height(height){}
-    
-    const double & getHeight() const 
-    {
-        return _height;
-    }
-    
-    const double & getWidth() const 
-    {
-        return _width;
-    }
-    
-    string draw()
-    {
-        string height = to_string(_height);
-        string width = to_string(_width);
-        string leftSide = "0 " + height + " rlineto \n";
-        string top = width + " 0 rlineto \n";
-        string rightSide = "0 -" + height + " rlineto \n";
-        return "newpath\n0 0 moveto\n" + leftSide + top + rightSide + "closepath\nstroke\n";
-    }
+    Rectangle(double width, double height);
+    const double & getHeight() const;
+    const double & getWidth() const;
+    string draw();
 private:
     double _height;
     double _width;
@@ -58,55 +40,11 @@ private:
 class Polygon : public BasicShapes
 {
 public:
-    virtual ~Polygon() {};
-    
-    Polygon(double numSides, double sideLength):_numberOfSides(numSides), _sideLength(sideLength)
-    {
-        if(fmod(_numberOfSides, 2) == 1)
-        {
-            _height = _sideLength*(1+cos(M_PI/_numberOfSides))/(2*sin(M_PI/_numberOfSides));
-            _width = (_sideLength * sin(M_PI*(_numberOfSides-1)/(2*_numberOfSides)))/(sin(M_PI/_numberOfSides));
-        }
-        else if(fmod(_numberOfSides, 4) == 0)
-        {
-            _height = _sideLength*(cos(M_PI/_numberOfSides))/(sin(M_PI/_numberOfSides));
-            _width = (_sideLength*cos(M_PI/_numberOfSides))/(sin(M_PI/_numberOfSides));
-        }
-        else
-        {
-            _height = _sideLength*(cos(M_PI/_numberOfSides))/(sin(M_PI/_numberOfSides));
-            _width = _sideLength/(sin(M_PI/_numberOfSides));
-        }
-    }
-    
-    const double & getHeight() const 
-    {
-        return _height;
-    }
-    
-    const double & getWidth() const 
-    {
-        return _width;
-    }
-    
-    string draw()
-    {
-        string sideLength = to_string(_sideLength);
-        string numberOfSides = to_string(int(_numberOfSides));
-        
-        double rotationalVariable = 180 - (((_numberOfSides - 2) * 180) /_numberOfSides);
-        
-        
-        double startingPos = (_width - _sideLength)/2;
-        string start = "newpath\n";
-        string translate = to_string(int(startingPos)) + " 0 translate \n";
-        string drawPolygon = "0 1 " + numberOfSides + "{\n0 0 moveto\n" +
-            sideLength + " 0 lineto\ncurrentpoint translate\n" +
-            to_string(rotationalVariable) + " rotate\n}for \nstroke \n";
-        
-        return "gsave \n" + start + translate + drawPolygon + "grestore \n\n";
-    }
-    
+    virtual ~Polygon();
+    Polygon(double numSides, double sideLength);
+    const double & getHeight() const;
+    const double & getWidth() const;
+    string draw();
 private:
     double _height;
     double _width;
@@ -117,22 +55,10 @@ private:
 class Square : public BasicShapes
 {
 public:
-    Square(double sideLength):_sideLength(sideLength) {}
-    
-    const double & getHeight() const 
-    {
-        return Polygon(4, _sideLength).getHeight();
-    }
-    
-    const double & getWidth() const 
-    {
-        return Polygon(4, _sideLength).getHeight();
-    }
-    
-    string draw()
-    {
-        return Polygon(4, _sideLength).draw();
-    }
+    Square(double sideLength);
+    const double & getHeight() const;
+    const double & getWidth() const;
+    string draw();
     
 private:
     double _sideLength;
@@ -141,22 +67,10 @@ private:
 class Triangle : public BasicShapes
 {
 public:
-    Triangle(double sideLength):_sideLength(sideLength) {}
-    
-    const double & getHeight() const 
-    {
-        return Polygon(3, _sideLength).getHeight();
-    }
-    
-    const double & getWidth() const 
-    {
-        return Polygon(3, _sideLength).getWidth();
-    }
-    
-    string draw()
-    {
-        return Polygon(3, _sideLength).draw();
-    }
+    Triangle(double sideLength);
+    const double & getHeight() const;
+    const double & getWidth() const;
+    string draw();
 private:
     double _sideLength;
 };
@@ -164,28 +78,10 @@ private:
 class Spacer : public BasicShapes
 {
 public:
-    Spacer(double width, double height):_width(width), _height(height){}
-    
-    const double & getHeight() const 
-    {
-        return _height;
-    }
-    
-    const double & getWidth() const 
-    {
-        return _width;
-    }
-    
-    string draw()
-    {
-        string height = to_string(_height);
-        string width = to_string(_width);
-        string leftSide = "0 " + height + " lineto \n";
-        string top = width + " " + height + " lineto \n";
-        string rightSide =  width + " 0 lineto \n";
-        return "newpath " + leftSide + top + rightSide + "closepath \n";
-        
-    }
+    Spacer(double width, double height);
+    const double & getHeight() const;
+    const double & getWidth() const;
+    string draw();
 private:
     double _height;
     double _width;
@@ -194,21 +90,10 @@ private:
 class Circle : public BasicShapes
 {
 public:
-    Circle(double radius):_radius(radius){}
-    const double & getHeight() const 
-    {
-        return _radius*2;
-    }
-    
-    const double & getWidth() const 
-    {
-        return getHeight();
-    }
-    string draw()
-    {
-        string radius = to_string(_radius);
-        return "newpath " + radius + " " + radius + " " + radius + " 0 360 arc stroke \n";
-    }
+    Circle(double radius);
+    const double & getHeight() const;
+    const double & getWidth() const;
+    string draw();
 private:
     double _radius;
 };
